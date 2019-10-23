8f0bf761ee530e2f736862c81d51a9453c1fcf22
Sitaram Mitharwal <simithar@microsoft.com>
2019-10-23 23:28:33 +0530

    security fixes

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8918881e79ed611b6eab3354e7490d26.js"
+  "/srmitharwal.github.io/precache-manifest.966586e94186ab86b7f4c157975fd09c.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/srmitharwal.github.io/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
