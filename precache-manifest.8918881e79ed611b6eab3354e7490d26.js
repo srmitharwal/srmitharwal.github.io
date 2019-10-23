e47ba544d801a16a7558ceba960f3414bf917809
Sitaram Mitharwal <simithar@microsoft.com>
2019-10-23 23:33:56 +0530

    Updates

---
+++ precache-manifest.8918881e79ed611b6eab3354e7490d26.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "40cc0571a717a45d7897",
+    "url": "/static/css/main.1a24cbd1.chunk.css"
+  },
+  {
+    "revision": "40cc0571a717a45d7897",
+    "url": "/static/js/main.40cc0571.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "ec14eb012e5d855c520e",
+    "url": "/static/js/2.ec14eb01.chunk.js"
+  },
+  {
+    "revision": "a1c4a63f688e8b2797a3fc360ec3d20f",
+    "url": "/index.html"
+  }
+];
