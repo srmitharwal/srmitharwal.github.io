51d842b16c64e45b68214e2045114fba04652a31
Sitaram Mitharwal <simithar@microsoft.com>
2019-10-23 22:40:06 +0530

    intital commit

---
+++ build/precache-manifest.a8daa65e3fcfcd46941c28494947b405.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "43a4babfcf7fd3d1aeec",
+    "url": "/srmitharwal.github.io/static/css/main.039ecac1.chunk.css"
+  },
+  {
+    "revision": "43a4babfcf7fd3d1aeec",
+    "url": "/srmitharwal.github.io/static/js/main.43a4babf.chunk.js"
+  },
+  {
+    "revision": "517ce79e57e0c965b9fa",
+    "url": "/srmitharwal.github.io/static/js/runtime~main.517ce79e.js"
+  },
+  {
+    "revision": "ec14eb012e5d855c520e",
+    "url": "/srmitharwal.github.io/static/js/2.ec14eb01.chunk.js"
+  },
+  {
+    "revision": "0eaa4bacbd196a7b3503c0821af377f8",
+    "url": "/srmitharwal.github.io/index.html"
+  }
+];
---
+++ src/components/timeline.jsx
@@ -0,0 +1,59 @@
+import React, { Component } from 'react'
+
+export default class Timeline extends Component {
+  render() {
+    return (
+      <div>
+        <section className="colorlib-experience" data-section="timeline">
+          <div className="colorlib-narrow-content">
+            <div className="row">
+              <div className="col-md-6 col-md-offset-3 col-md-pull-3 animate-box" data-animate-effect="fadeInLeft">
+                <span className="heading-meta">highlights</span>
+                <h2 className="colorlib-heading animate-box">Timeline</h2>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-md-12">
+                <div className="timeline-centered">
+                  <article className="timeline-entry animate-box" data-animate-effect="fadeInTop">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-4">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>Graduation at IIT Guwahati<span>2013-2017</span></h2>
+                        <p>I did graduation from IIT guwahti with major in Computer Science and Technology. I have taken courses like DSA, Game Theory, Discrete Mathematics, Computer Networks,Game Theory,Operating Systems, Advance machine learning etc over the years and have good understanding of these subjects.</p>
+                      </div>
+                    </div>
+                  </article>
+                  <article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-5">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>Higher Education <span>2010-2012</span></h2>
+                        <p>I have completed my higher secondary education from Prince Academy Sikar with 87% with Maths as major. After higher school, I took a one year drop to prepare for JEE. Successfully cracked the JEE Advance with rank 607 and JEE Mains with rank 330.</p> 
+                      </div>
+                    </div>
+                  </article>
+                  <article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-5">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>Primary and Secondary Education <span>2000-2010</span></h2>
+                        <p>I have completed my secondary education from BSER Board with 91%. </p> 
+                      </div>
+                    </div>
+                  </article>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+    )
+  }
+}
